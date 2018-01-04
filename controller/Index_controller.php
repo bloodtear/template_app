5650c38619c91e8133b0be39e729db0d2278fdc0
bloodtear <xiaoyu1120520@126.com>
2018-01-04 18:27:44 +0800

    配合后台修改内容

--- controller/Index_controller.php
+++ controller/Index_controller.php
@@ -11,7 +11,24 @@ class Index_controller {
 
   function index_action() {
       $tpl = new Tpl('index/header', 'index/footer');
+
+      $ar = array(
+        "a" => 2,
+        "b" => array(
+          "c" => "d",
+          "e" => array(
+            "jh" => 123,
+            "gj" => 223
+          )
+        )
+      );
+      $xy = new stdClass();
+      $xy->name = 'xiaoyu';
+      $xy->year = 30;
       $tpl->set("id", 123);
+      $tpl->set("name", "xiaoyu");
+      $tpl->set("xy", $xy);
+      $tpl->set("ar", $ar);
       $tpl->view('index/index');
   }
 
