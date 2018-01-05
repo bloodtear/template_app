47fa7854da7f5e8aa145c311199e22bc2de6cf27
bloodtear <xiaoyu1120520@126.com>
2018-01-05 17:57:49 +0800

    修改ajax调用函数，对于错误给出提示

--- controller/admin/v1/User_controller.php
+++ controller/admin/v1/User_controller.php
@@ -14,6 +14,7 @@ class User_controller {
   }
 
   function login_ajax(){
+    echo 123;
 
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 function __ajax(action, data, callback) {
 
   $.ajax({
-    type: "GET",
+    type: "post",
     url: 'ajax.php?action=' + action,
     data: data,
     dataType: "json",
     success: function(data){
       console.log(data);
       callback(data);
+    },
+    error: function (object, info) {
+      console.log('errorinfo:' + info);
+      console.log(object.responseText);
     }
   });
 
